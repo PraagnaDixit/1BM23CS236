22ba3d501dc3fa0fc1f961fa20d80eb76e61ac50
PraagnaDixit <praagnadixit.cs23@bmsce.ac.in>
2024-10-16 10:18:07 +0530

    Add files via upload

---
+++ infix.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <ctype.h>  // for isalnum
+#include <string.h> // for strlen
+
+char stack[50];
+int top = -1;
+
+void push(char x) {
+    stack[++top] = x;
+}
+
+char pop() {
+    if (top == -1) {
+        return -1;
+    } else {
+        return stack[top--];
+    }
+}
+
+int priority(char x) {
+    if (x == '(') {
+        return 0;
+    } else if (x == '+' || x == '-') {
+        return 1;
+    } else if (x == '*' || x == '/') {
+        return 2;
+    }
+    return -1;
+}
+
+int main() {
+    char exp[20];
+    printf("Enter your expression: ");
+    scanf("%s", exp);
+
+    char *e = exp;
+    char x;
+
+    while (*e != '\0') {
+        if (isalnum(*e)) {
+            printf("%c", *e);
+        } else if (*e == '(') {
+            push(*e);
+        } else if (*e == ')') {
+            while ((x = pop()) != '(') {
+                printf("%c", x);
+            }
+        } else {
+            while (top != -1 && priority(stack[top]) >= priority(*e)) {
+                printf("%c", pop());
+            }
+            push(*e);
+        }
+        e++;
+    }
+
+
+    while (top != -1) {
+        printf("%c", pop());
+    }
+
+    printf("\n");
+    return 0;
+}
